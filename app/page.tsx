4844fce0a16ac4fca80c26150aa5e60f83a5e86a
Mathew <mateoicecua2000@gmail.com>
2025-04-17 13:34:02 -0500

    feat(sections): add AboutMe section and Projects & AboutMe routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { AboutMeSection } from './(pages)/about-me/components/AboutMeSection'
 import { Hero } from './components/Hero'
 
 export default function Home() {
-  return <Hero />
+  return (
+    <>
+      <Hero />
+      <AboutMeSection />
+    </>
+  )
 }
